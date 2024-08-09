c82c8e0d319d39fe89abdbccb260e92a256c74fe
Sachintech-github <95368495+Sachintech-github@users.noreply.github.com>
2024-08-09 10:17:52 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,3 +16,4 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
